97dbe90f2b2a5d27e0ee7bd4adede0bd9b888f36
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-10-27 13:21:47 +0900

    회원가입 기능 구현

---
+++ a_pjt/accounts/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+    users = get_user_model().objects.all()  # 저장된 모든 User를 가져온다.
+    context = {
+        "users": users,
+    }
+    return render(request, "accounts/index.html", context)
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # 회원가입과 동시에 로그인을 시키기 위해
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect("accounts:index")
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "accounts:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:login")
+
+
+@login_required
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect("accounts:login")
+
+
+@login_required
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/update.html", context=context)
+
+
+@login_required
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/change_password.html", context)
