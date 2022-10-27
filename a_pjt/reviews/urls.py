8a01674e48d712552fdb8c931c656eb98a4fc4a9
an dong woo <akskwha@naver.com>
2022-10-27 14:26:54 +0900

    코드 수정

--- a_pjt/reviews/urls.py
+++ a_pjt/reviews/urls.py
@@ -6,6 +6,7 @@ app_name = "reviews"
 
 urlpatterns = [
     path("<int:review_pk>/", views.detail, name="detail"),
+    path("create/", views.create, name="create"),
     path('<int:pk>/comments/', views.comment_create, name='comment_create'),
     path('<int:article_pk>/comments/<int:comment_pk>/comment_delete/', views.comment_delete, name='comment_delete'),
 ]
--- a_pjt/reviews/views.py
+++ a_pjt/reviews/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Review
-from .forms import CommentForm
+from .forms import Commentform
 # Create your views here.
 
 def detail(request, review_pk):
     review = Review.objects.get(pk=review_pk)
-    commet_form = Commetform()
+    commet_form = Commentform()
     context={
         'review':review,
         'comment_form':commet_form,
@@ -15,7 +15,7 @@ def detail(request, review_pk):
 
 def comment_create(request, pk):
     review = Review.objects.get(pk=pk)
-    comment_form = CommetForm(request.POST)
+    comment_form = Commentform(request.POST)
     if comment_form.is_valid():
         comment = comment_form.save(commit=False)
         comment.review = review
@@ -24,7 +24,7 @@ def comment_create(request, pk):
     return redirect('reviews:detail', review.pk )
 
 def comment_delete(request, article_pk, comment_pk):
-    comment = CommetForm.objects.get(pk=comment_pk)
+    comment = Commentform.objects.get(pk=comment_pk)
     comment.delete()
-    return redirect('reviews:detail', review_pk)
+    return redirect('reviews:detail', article_pk)
 
